7400fae648192c7af918687b8d747a0feaa3007e
camilarsobral <camilarsobral@hotmail.com>
2023-06-02 13:59:48 -0300

    footer

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ console.log(menuItems)
 
 gallery.addEventListener('click', (e) => {
 
-    // console.log(e)
-
     if (e.target.classList.contains('filter')) {
         gallery.querySelector('.active').classList.remove('active')
         e.target.classList.add('active')
@@ -27,33 +25,10 @@ gallery.addEventListener('click', (e) => {
 
 })
 
-// document.querySelector('.active').classList.remove('active')
-// e.target.classList.add('active')
+const appearOnScreen = {
+    origin: "top",
+    distance: "100px",
+    duration: 3000,
+}
 
-
-// const filteredButtons = document.querySelectorAll('.container-buttons button')
-// const filterableCards = document.querySelectorAll('.filterable-cards .card')
-
-// const filterCards = button => {
-//     document.querySelector('.active').classList.remove('active')
-//     button.target.classList.add('active')
-
-//     filterableCards.forEach(card => {
-//         card.classList.add('hide')
-
-//         if (card.dataset.name === button.target.dataset.name || button.target.dataset.name === 'all')
-//             card.classList.remove('hide')
-
-
-//     })
-// }
-
-// filteredButtons.forEach(button => button.addEventListener('click', filterCards))
-
-// const animation = {
-//     origin: "top",
-//     distance: "100px",
-//     duration: 4000,
-// }
-
-// ScrollReveal().reveal('.container-all', animation)
+ScrollReveal().reveal('.container-all', appearOnScreen)
